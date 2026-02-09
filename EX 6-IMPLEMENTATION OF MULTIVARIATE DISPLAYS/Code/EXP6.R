af39e7749ce993cb9bb982aea4d0b259dd280731
vairavanathan63 <vairavanathan7603@gmail.com>
2026-02-09 10:22:50 +0530

    Add files via upload

---
+++ EX 6-IMPLEMENTATION OF MULTIVARIATE DISPLAYS/Code/EXP6.R
@@ -0,0 +1,51 @@
+# Roll No: 23BAD306
+# Multivariate Analysis
+
+# Load required libraries
+library(ggplot2)
+library(GGally)
+library(dplyr)
+
+# Load dataset
+retail_data <- read.csv("C:\\Users\\navin\\Downloads\\6.retail_business.csv")
+
+# Remove missing values
+retail_data <- na.omit(retail_data)
+
+# -----------------------------------------
+# 1. Parallel Coordinate Plot (Sales & Profit)
+# -----------------------------------------
+
+ggparcoord(
+  data = retail_data,
+  columns = which(names(retail_data) %in% c("Sales", "Profit")),
+  groupColumn = which(names(retail_data) == "Customer_Segment"),
+  scale = "uniminmax",
+  showPoints = TRUE
+) +
+  labs(
+    title = "Parallel Coordinate Plot for Sales and Profit",
+    color = "Customer Segment"
+  ) +
+  theme_minimal()
+
+# -----------------------------------------
+# 2. Bubble Chart: Sales vs Profit
+# -----------------------------------------
+
+ggplot(
+  retail_data,
+  aes(
+    x = Sales,
+    y = Profit,
+    size = Sales,
+    color = Customer_Segment
+  )
+) +
+  geom_point(alpha = 0.7) +
+  labs(
+    title = "Bubble Chart: Sales vs Profit",
+    x = "sales",
+    y = "profit"
+  )+
+  theme_minimal()
